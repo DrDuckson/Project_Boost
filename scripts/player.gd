5418f0abfcd891243eef0fa78b5236098c3d9045
DrDuckson <gorealex95@web.de>
2024-06-13 23:49:55 +0200

    Player scene with script for movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,22 @@
-extends Node3D
+extends RigidBody3D
 
 func _ready() -> void:
-	print("Hello Mom!")
-	print("Don't panic!")
-	print(42)
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_up"):
+		apply_central_force(basis.y * delta * 1000)
+	if Input.is_action_pressed("move_left"):
+		apply_torque(Vector3(0.0, 0.0, 100.0*delta))
+	if Input.is_action_pressed("move_right"):
+		apply_torque(-Vector3(0.0, 0.0, 100.0*delta))
+		
+func _physics_process(delta: float) -> void:
+	pass
+	
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print(body.name)
+	if "Hazard" in body.get_groups():
+		print("you hit the " + body.name + "!")
+		get_tree().reload_current_scene()
